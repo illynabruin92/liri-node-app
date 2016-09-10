268a2f2d817128d7136c888fec068dec35a95cc6
illynabruin92 <alvaivette@g.ucla.edu>
2016-09-09 22:40:17 -0700

    bones of spotify and omdb

--- liri.js
+++ liri.js
@@ -93,7 +93,21 @@ client.post('statuses/update', {status: 'Brought to you by LIRI'}, function(erro
 //*********************************************************
 var spotify = require('spotify');
 
+spotify.search({ type: 'track', query: 'The Sign' }, function(err, data) {
+    if ( err ) {
+        console.log('Error occurred: ' + err);
+        return;
+    }
 
+});
+
+
+//    * The song's name
+//    * A preview link of the song from Spotify
+//    * The album that the song is from
+
+// * if no song is provided then your program will default to
+//    * "The Sign" by Ace of Base
 
 
 
@@ -103,4 +117,9 @@ var spotify = require('spotify');
 
 //                     OMDB SECTION
 
-//**********************************************************
+//**********************************************************var request = require('request');
+request('http://www.omdbapi.com', function (error, response, body) {
+  if (!error && response.statusCode == 200) {
+    console.log('Fatal Error.')
+  }
+})
